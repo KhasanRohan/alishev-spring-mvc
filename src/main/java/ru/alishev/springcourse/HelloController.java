0d96528cf6ca65ff27d8c44f4689416abc93419c
KhasanRohan <g.nurudinov@mail.ru>
2025-08-03 13:48:32 +0300

    commented hello controller

--- src/main/java/ru/alishev/springcourse/HelloController.java
+++ src/main/java/ru/alishev/springcourse/HelloController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HelloController {
 
-    @GetMapping("/hello-world")
-    public String sayHello() {
-        return "hello_world";
-    }
+//    @GetMapping("/hello-world")
+//    public String sayHello() {
+//        return "hello_world";
+//    }
 }
